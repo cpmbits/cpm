27dd874faef17e8e4fb69d4636dbce8395fa701d
Jordi Sánchez <jordi@pop-os.localdomain>
2020-03-13 20:43:03 +0100

    implemented new module install service

--- cpm/domain/install_service.py
+++ cpm/domain/install_service.py
@@ -1,2 +1,16 @@
+class InstallService(object):
+    def __init__(self, project_loader, plugin_installer, cpm_hub_connector):
+        self.cpm_hub_connector = cpm_hub_connector
+        self.project_loader = project_loader
+        self.plugin_installer = plugin_installer
+
+    def install(self, plugin_name):
+        project = self.project_loader.load()
+        plugin_download = self.cpm_hub_connector.download_plugin(plugin_name)
+        self.plugin_installer.install(plugin_download)
+        project.add_plugin(plugin_download.plugin)
+        self.project_loader.save(project)
+
+
 class PluginNotFound(RuntimeError):
     pass
---
+++ cpm/domain/plugin_download.py
@@ -0,0 +1,5 @@
+class PluginDownload(object):
+    def __init__(self, name, version):
+        self.version = version
+        self.name = name
+
