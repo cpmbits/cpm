e057051f4e711ad8d08e186f9c25331cb0642636
Jordi Sánchez <jordi@pop-os.localdomain>
2020-04-02 19:37:04 +0200

    ignore security in connections to CPM Hub

--- cpm/infrastructure/http_client.py
+++ cpm/infrastructure/http_client.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
+from urllib3.exceptions import InsecureRequestWarning
 import requests
 
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 @dataclass
 class HttpResponse:
@@ -10,18 +13,18 @@ class HttpResponse:
 
 def post(url, data=None, headers=None, files=None):
     try:
-        requests.post(url, files=files, data=data, headers=headers)
-    except requests.exceptions.ConnectionError:
+        requests.post(url, files=files, data=data, headers=headers, verify=False)
+    except requests.exceptions.ConnectionError as e:
         raise HttpConnectionError()
 
 
 def put(url, data=None, headers=None, files=None):
-    requests.put(url, files=files, data=data, headers=headers)
+    requests.put(url, files=files, data=data, headers=headers, verify=False)
 
 
 def get(url, data=None, headers=None, files=None):
     try:
-        response = requests.get(url, files=files, data=data, headers=headers)
+        response = requests.get(url, files=files, data=data, headers=headers, verify=False)
         return HttpResponse(response.status_code, response.text)
     except requests.exceptions.ConnectionError:
         raise HttpConnectionError(url)
