8fe6647d43de398ab150bf833f40dadc0c8fe27b
Jamo <jamofer@gmail.com>
2020-04-30 00:48:40 +0200

    Adapt setup to PEP517 with setuptools backend

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-# -*- coding: utf-8 -*-
-
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="cpm-cli",
-    version="0.4",
-    scripts=['scripts/cpm'],
-    author="Jordi Sánchez",
-    description="Chromos Package Manager",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jorsanpe/cpm",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-)
+setuptools.setup(setup_requires=['setuptools>=36.6.0'])
