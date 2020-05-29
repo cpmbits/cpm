08d0e061635aafe25af10e4952441cdb09afc222
Jordi Sánchez <jorsanpe@gmail.com>
2020-05-29 09:15:53 +0200

    Do not re-install up to date bits (#132)
    Co-authored-by: Jordi Sánchez <jordi.sanchez@casa-systems.com>

--- cpm/domain/bit.py
+++ cpm/domain/bit.py
@@ -1,13 +1,13 @@
 class Bit(object):
-    def __init__(self, name):
+    def __init__(self, name, version=''):
         self.name = name
-        self.version = ""
+        self.version = version
         self.sources = []
         self.packages = []
         self.include_directories = []
 
     def __eq__(self, other):
-        return self.name == other.name
+        return self.name == other.name and self.version == other.version
 
     def add_include_directory(self, directory):
         self.include_directories.append(directory)
